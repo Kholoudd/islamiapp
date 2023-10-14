d387ead4124093931dc25ba62f2d3b3430b689ac
Kholoud <kholoudemad551@gmail.com>
2023-10-15 01:39:46 +0300

    provider for language and theme mode

---
+++ lib/core/provider/applicationProvider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AppPovider extends ChangeNotifier {
+  ThemeMode currentTheme = ThemeMode.dark;
+
+  String currentLocal = "en";
+
+  changeLanguage(String newLanguage) {
+    if (currentLocal == newLanguage) return;
+    currentLocal = newLanguage;
+    notifyListeners();
+  }
+
+  chanageTheme(ThemeMode newTheme) {
+    if (currentTheme == newTheme) return;
+    currentTheme = newTheme;
+    notifyListeners();
+  }
+
+  bool isDark() {
+    return currentTheme == ThemeMode.dark;
+  }
+
+  String background() {
+    return isDark()
+        ? "assets/images/dark_bg.png"
+        : "assets/images/default_bg.png";
+  }
+
+  String sebha() {
+    return isDark()
+        ? "assets/images/head_sebha_dark.png"
+        : "assets/images/head_sebha_logo.png";
+  }
+
+  String sebhaBody() {
+    return isDark()
+        ? "assets/images/body_sebha_dark.png"
+        : "assets/images/body_sebha_logo.png";
+  }
+
+  String splashBackground() {
+    return isDark()
+        ? "assets/images/backgrounddark.png"
+        : "assets/images/splashPage.png";
+  }
+}
